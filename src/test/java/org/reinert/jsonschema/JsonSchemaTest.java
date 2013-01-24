8f6b09d53d0fa63322ab9a8b39bc87f410f8b1bd
Onezino Moreira <zinogabriel@gmail.com>
2013-01-23 22:59:43 -0300

    Cria teste inicial para o gerador

---
+++ src/test/java/org/reinert/jsonschema/JsonSchemaTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.reinert.jsonschema;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author heatbr
+ */
+public class JsonSchemaTest extends TestCase {
+    
+    public JsonSchemaTest(String testName) {
+        super(testName);
+    }
+    
+    public void testGenerateSchema() {
+        class PojoNumber {
+            @SchemaProperty(title = "number")
+            public int number;
+            public PojoNumber(int n) {
+                this.number = n;
+            }
+        }
+        
+        JsonSchema s = JsonSchema.generateSchema(PojoNumber.class);
+        System.out.println(s.toString());
+        assertTrue(
+                s.hasProperties());
+    }
+}
