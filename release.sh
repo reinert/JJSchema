80001cc6b544038a8af0bcd45af476f995a8ad5c
Danilo Reinert <daniloreinert@gmail.com>
2017-05-28 15:43:47 -0300

    release script enhancement

--- release.sh
+++ release.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
-# usage: sh release.sh 0.1.0 0.2.0-SNAPSHOT
 
-if [ -n "$1" ] && [ -n "$2" ]; then
-  # update version to release version
-  mvn versions:set -DnewVersion=$1
-  mvn versions:commit
-  # update tag to release version
-  sh ./tag.sh $1 --readme
-  # commit
-  git add .
-  git commit -m "Release $1"
-  # deploy
-  mvn clean deploy -Prelease
-  # tag
-  git tag -a JJSchema-$1 -m "JJSchema v$1"
-  git push origin JJSchema-$1
-  # update version to next snapshot
-  mvn versions:set -DnewVersion=$2
-  mvn versions:commit
-  # update tag to next snapshot
-  sh ./tag.sh HEAD
-  # commit
-  git add .
-  git commit -m "Start $2 development"
-  git push origin master
-else
-  echo "USAGE"
-  echo "1)  Release version: sh release.sh {release-version} {next-snapshot}"
-  echo "            Example: sh release.sh 0.1.0 0.2.0-SNAPSHOT"
-  echo "2) Release snapshot: sh release.sh snapshot"
-  echo "        Alternative: sh release.sh current"
-fi
+# set version variables
+source ./VERSION
+
+MINOR=$((MINOR + 1))
+
+CURRENT="$MAJOR.$MINOR"
+SNAPSHOT="$MAJOR.$((MINOR + 1))-SNAPSHOT"
+
+# update version to release version
+mvn versions:set -DnewVersion=$CURRENT
+mvn versions:commit
+
+# update tag to release version
+sh ./tag.sh $CURRENT --readme
+
+# commit
+git add .
+git commit -m "Release $CURRENT"
+
+# deploy
+mvn clean deploy -Prelease
+
+# tag
+git tag -a JJSchema-$CURRENT -m "JJSchema v$CURRENT"
+git push origin JJSchema-$CURRENT
+
+# update version to next snapshot
+mvn versions:set -DnewVersion=$SNAPSHOT
+mvn versions:commit
+
+# update tag to next snapshot
+sh ./tag.sh HEAD
+
+# commit
+git add .
+git commit -m "Start $SNAPSHOT development"
+git push origin master
+
+# update VERSION file
+echo "MAJOR=$MAJOR" > ./VERSION
+echo "MINOR=$MINOR" >> ./VERSION
+
