f77757aeb429eb5f6dba1fd2f74b82a0e251982f
while(true) <33146321+WhileTrueEndWhile@users.noreply.github.com>
2018-07-09 16:08:50 +0200

    Added XPropertiesWriter.java

---
+++ src/main/java/com/github/reinert/jjschema/xproperties/XPropertiesWriter.java
@@ -0,0 +1,31 @@
+package com.github.reinert.jjschema.xproperties;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * 
+ * X Properties Writer
+ * 
+ * 
+ * @author WhileTrueEndWhile
+ * 
+ */
+public interface XPropertiesWriter {
+
+    /**
+     * 
+     * Writes X Properties into schema.
+     * 
+     * 
+     * @param schema
+     *            Destination schema.
+     * 
+     * @param properties
+     *            List of X Properties.
+     * 
+     */
+    void writeXProperties(ObjectNode schema, List<XProperty> properties);
+
+}
