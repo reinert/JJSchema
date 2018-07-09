4cb7b3e6c5f4809ec73bf7a9b7acb1f240426be2
while(true) <33146321+WhileTrueEndWhile@users.noreply.github.com>
2018-07-09 20:53:48 +0200

    Fine Tuning

--- src/main/java/com/github/reinert/jjschema/xproperties/impl/DefaultXPropertiesWriter.java
+++ src/main/java/com/github/reinert/jjschema/xproperties/impl/DefaultXPropertiesWriter.java
@@ -33,64 +33,64 @@ public class DefaultXPropertiesWriter implements XPropertiesWriter {
     public void writeXProperties(ObjectNode schema, List<XProperty> properties) {
         properties.forEach(property -> {
             final List<Object> propertyPath = property.getPropertyPath();
-
-            final Object propertyKeyN = propertyPath.get(propertyPath.size() - 1);
+            final Object propertyKey = propertyPath.get(propertyPath.size() - 1);
             final Object propertyValue = property.getPropertyValue();
 
-            JsonNode ptrN = schema;
-
+            JsonNode outerPtr = schema;
             for (int i = 0; i < propertyPath.size() - 1; ++i) {
 
-                final Object keyN = propertyPath.get(i);
-                final Object keyI = propertyPath.get(i + 1);
+                final Object outerKey = propertyPath.get(i);
+                final Object innerKey = propertyPath.get(i + 1);
 
-                JsonNode ptrI;
-
-                if (keyN instanceof Integer) {
-                    ptrI = ((ArrayNode) ptrN).get((Integer) keyN);
+                JsonNode innerPtr;
+                if (outerKey instanceof Integer) {
+                    innerPtr = ((ArrayNode) outerPtr).get((Integer) outerKey);
                 } else {
-                    ptrI = ((ObjectNode) ptrN).get((String) keyN);
+                    innerPtr = ((ObjectNode) outerPtr).get((String) outerKey);
                 }
 
-                processNode(ptrN, keyN, ptrI, keyI);
+                prepareNodeStructure(outerPtr, outerKey, innerPtr, innerKey);
 
-                if (keyN instanceof Integer) {
-                    ptrI = ((ArrayNode) ptrN).get((Integer) keyN);
+                if (outerKey instanceof Integer) {
+                    innerPtr = ((ArrayNode) outerPtr).get((Integer) outerKey);
                 } else {
-                    ptrI = ((ObjectNode) ptrN).get((String) keyN);
+                    innerPtr = ((ObjectNode) outerPtr).get((String) outerKey);
                 }
-
-                ptrN = ptrI;
+                outerPtr = innerPtr;
             }
-            if (propertyKeyN instanceof Integer) {
-                try {
-                    if (propertyValue == null) {
-                        final Method remove = ArrayNode.class.getMethod("remove", int.class);
-                        remove.invoke(ptrN, (Integer) propertyKeyN);
-                    } else {
-                        final Method insert = ArrayNode.class.getMethod("insert", int.class, propertyValue.getClass());
-                        insert.invoke(ptrN, (Integer) propertyKeyN, propertyValue);
-                    }
-                } catch (ReflectiveOperationException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                try {
-                    if (propertyValue == null) {
-                        final Method remove = ObjectNode.class.getMethod("remove", String.class);
-                        remove.invoke(ptrN, (String) propertyKeyN);
-                    } else {
-                        final Method put = ObjectNode.class.getMethod("put", String.class, propertyValue.getClass());
-                        put.invoke(ptrN, (String) propertyKeyN, propertyValue);
-                    }
-                } catch (ReflectiveOperationException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+            setNodeValue(outerPtr, propertyKey, propertyValue);
         });
     }
 
-    private void processNode(JsonNode outerPtr, Object outerKey, JsonNode innerPtr, Object innerKey) {
+    private static void setNodeValue(JsonNode ptr, Object key, Object value) {
+        if (key instanceof Integer) {
+            try {
+                if (value == null) {
+                    final Method remove = ArrayNode.class.getMethod("remove", int.class);
+                    remove.invoke(ptr, (Integer) key);
+                } else {
+                    final Method insert = ArrayNode.class.getMethod("insert", int.class, value.getClass());
+                    insert.invoke(ptr, (Integer) key, value);
+                }
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            try {
+                if (value == null) {
+                    final Method remove = ObjectNode.class.getMethod("remove", String.class);
+                    remove.invoke(ptr, (String) key);
+                } else {
+                    final Method put = ObjectNode.class.getMethod("put", String.class, value.getClass());
+                    put.invoke(ptr, (String) key, value);
+                }
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void prepareNodeStructure(JsonNode outerPtr, Object outerKey, JsonNode innerPtr, Object innerKey) {
         if (innerKey instanceof Integer) {
             if (!(innerPtr instanceof ArrayNode)) {
                 final ArrayNode arrayNode = MAPPER.createArrayNode();
