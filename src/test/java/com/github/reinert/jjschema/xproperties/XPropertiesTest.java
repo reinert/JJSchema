436da8f36e2b8bc8354821049ab428e3ce9f7b37
while(true) <33146321+WhileTrueEndWhile@users.noreply.github.com>
2018-07-09 16:14:42 +0200

    Added XPropertiesTest.java

---
+++ src/test/java/com/github/reinert/jjschema/xproperties/XPropertiesTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2014, Danilo Reinert (daniloreinert@growbit.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of both licenses is available under the src/resources/ directory of
+ * this project (under the names LGPL-3.0.txt and ASL-2.0.txt respectively).
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.reinert.jjschema.xproperties;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.reinert.jjschema.Attributes;
+import com.github.reinert.jjschema.exception.UnavailableVersion;
+import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author reinert
+ */
+public class XPropertiesTest extends TestCase {
+
+    static ObjectMapper MAPPER = new ObjectMapper();
+    JsonSchemaFactory schemaFactory = new JsonSchemaV4Factory();
+
+    public XPropertiesTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Test the scheme generate following a scheme source, avaliable at
+     * http://json-schema.org/examples.html the output should match the example.
+     *
+     * @throws java.io.IOException
+     * @throws com.fasterxml.jackson.core.JsonProcessingException
+     *
+     */
+    public void testGenerateSchema() throws UnavailableVersion, JsonProcessingException, IOException {
+
+        final InputStream in = XPropertiesTest.class.getResourceAsStream("/xproperties_exmaple.json");
+        if (in == null)
+            throw new IOException("resource not found");
+        JsonNode fromResource = MAPPER.readTree(in);
+        JsonNode fromJavaType = schemaFactory.createSchema(SimpleExample.class);
+        System.out.println(MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(fromJavaType));
+
+        assertEquals(fromResource, fromJavaType);
+    }
+
+    @Attributes(title = "Example Schema", xProperties = {
+        "fieldsets.0.fields.0 = java.lang.String:firstName",
+        "fieldsets.0.fields.1 = java.lang.String:lastName",
+        "fieldsets.1.fields.0 = java.lang.String:age"
+    })
+    static class SimpleExample {
+        @Attributes(title = "First Name", required = true, xProperties = {
+            "widget.id = java.lang.String:string"
+        })
+        private String firstName;
+        @Attributes(title = "Last Name", required = true, xProperties = {
+            "widget.id = java.lang.String:string"
+        })
+        private String lastName;
+        @Attributes(title = "Age in years", minimum = 0, xProperties = {
+            "widget.id = java.lang.String:number"
+        })
+        private int age;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
