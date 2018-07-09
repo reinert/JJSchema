1f56650d0c838971da953f74001289927497f002
while(true) <33146321+WhileTrueEndWhile@users.noreply.github.com>
2018-07-09 21:32:28 +0200

    Should be done

--- src/main/java/com/github/reinert/jjschema/xproperties/XProperty.java
+++ src/main/java/com/github/reinert/jjschema/xproperties/XProperty.java
@@ -1,35 +1,35 @@
 package com.github.reinert.jjschema.xproperties;
-+
-+import java.util.List;
-+
-+/**
-+ * 
-+ * X Property
-+ * 
-+ * 
-+ * @author WhileTrueEndWhile
-+ * 
-+ */
-+public interface XProperty {
-+
-+    /**
-+     * 
-+     * Gets the property path.
-+     * 
-+     * 
-+     * @return A List of integers and strings.
-+     * 
-+     */
-+    List<Object> getPropertyPath();
-+
-+    /**
-+     * 
-+     * Gets the property value.
-+     * 
-+     * 
-+     * @return A boolean, an integer, a double or a string.
-+     * 
-+     */
-+    Object getPropertyValue();
-+
-+}
+
+import java.util.List;
+
+/**
+ * 
+ * X Property
+ * 
+ * 
+ * @author WhileTrueEndWhile
+ * 
+ */
+public interface XProperty {
+
+    /**
+     * 
+     * Gets the property path.
+     * 
+     * 
+     * @return A List of integers and strings.
+     * 
+     */
+    List<Object> getPropertyPath();
+
+    /**
+     * 
+     * Gets the property value.
+     * 
+     * 
+     * @return A boolean, an integer, a double or a string.
+     * 
+     */
+    Object getPropertyValue();
+
+}
