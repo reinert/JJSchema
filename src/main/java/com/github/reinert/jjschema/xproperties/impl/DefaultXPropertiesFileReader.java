6f5a003d9fc8e6e27963feb420e4bd434eb6849f
while(true) <33146321+WhileTrueEndWhile@users.noreply.github.com>
2018-07-13 15:59:49 +0200

    Refactoring

--- src/main/java/com/github/reinert/jjschema/xproperties/impl/DefaultXPropertiesFileReader.java
+++ src/main/java/com/github/reinert/jjschema/xproperties/impl/DefaultXPropertiesFileReader.java
@@ -1,26 +1,20 @@
 package com.github.reinert.jjschema.xproperties.impl;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.AccessibleObject;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Objects;
-import java.util.Properties;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.reinert.jjschema.xproperties.XProperties;
 import com.github.reinert.jjschema.xproperties.XPropertiesReader;
 import com.github.reinert.jjschema.xproperties.XProperty;
 
+/**
+ * X Properties File Reader Implementation
+ * 
+ * @author WhileTrueEndWhile
+ */
 public class DefaultXPropertiesFileReader implements XPropertiesReader {
-
-    //
-    // Errors
-    //
-    private static final String ERROR_RESOURCE_NOT_FOUND = "Could not find resource";
-    private static final String ERROR_RESOURCE_IO_ERROR = "Could not load resource";
 
     /**
      * Reads X Properties from a class.
@@ -35,7 +29,7 @@ public class DefaultXPropertiesFileReader implements XPropertiesReader {
         type = Objects.requireNonNull(type);
 
         final XProperties attributes = type.getAnnotation(XProperties.class);
-        return readXProperties(attributes);
+        return ReadImpl.readXProperties(attributes);
     }
 
     /**
@@ -52,7 +46,7 @@ public class DefaultXPropertiesFileReader implements XPropertiesReader {
         accessibleObj = Objects.requireNonNull(accessibleObj);
 
         final XProperties attributes = accessibleObj.getAnnotation(XProperties.class);
-        return readXProperties(attributes);
+        return ReadImpl.readXProperties(attributes);
     }
 
     /**
@@ -64,49 +58,10 @@ public class DefaultXPropertiesFileReader implements XPropertiesReader {
      * @param schema
      *            Schema of the class containing the fields to read from.
      * 
-     * @return A list of X Properties
+     * @return A list of X Properties.
      */
-
     @Override
     public List<XProperty> readXProperties(Class<?> type, ObjectNode schema) {
         throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Reads X Properties from an annotation instance.
-     * 
-     * @param attributes
-     *            An annotation instance.
-     * 
-     * @return A list of X Properties.
-     */
-    public static List<XProperty> readXProperties(XProperties attributes) {
-        final List<XProperty> listOfProperties = new ArrayList<>();
-        if (attributes == null || attributes.files() == null) {
-            return listOfProperties;
-        }
-        final String[] xPropertiesFiles = attributes.files();
-        for (int i = 0; i < xPropertiesFiles.length; ++i) {
-            final String xPropertiesFileName = xPropertiesFiles[i];
-            final InputStream xPropertiesFile = System.class.getResourceAsStream(xPropertiesFileName);
-            if (xPropertiesFile == null) {
-                throw new IllegalArgumentException(ERROR_RESOURCE_NOT_FOUND + ": " + xPropertiesFileName);
-            }
-            final Properties properties = new Properties();
-            try {
-                properties.load(xPropertiesFile);
-            } catch (IOException e) {
-                throw new IllegalArgumentException(ERROR_RESOURCE_IO_ERROR + ": " + xPropertiesFileName, e);
-            }
-
-            final Enumeration<?> propertyNames = properties.propertyNames();
-            while (propertyNames.hasMoreElements()) {
-                final String propertyPath = (String) propertyNames.nextElement();
-                final String propertyValue = properties.getProperty(propertyPath);
-                final XProperty xProperty = DefaultXPropertiesReader.readProperty(propertyPath, propertyValue);
-                listOfProperties.add(xProperty);
-            }
-        }
-        return listOfProperties;
     }
 }
