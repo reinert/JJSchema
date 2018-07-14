4892392305cd54286b048445f539d3f43a552069
while(true) <33146321+WhileTrueEndWhile@users.noreply.github.com>
2018-07-14 14:04:26 +0200

    More tests...

--- src/test/java/com/github/reinert/jjschema/xproperties/ValidPropertyTest.java
+++ src/test/java/com/github/reinert/jjschema/xproperties/ValidPropertyTest.java
@@ -19,16 +19,83 @@ public class ValidPropertyTest extends TestCase {
         super(testName);
     }
 
+    public void testNullValueOnClass() throws Throwable {
+        JsonNode fromResource = MAPPER.readTree("{}");
+        JsonNode fromJavaType = schemaFactory.createSchema(NullValueOnClassExample.class);
+        System.out.println(MAPPER.writeValueAsString(fromJavaType));
+        assertEquals(fromResource, fromJavaType);
+    }
+
+    public void testNullValueOnField() throws Throwable {
+        JsonNode fromResource = MAPPER.readTree(
+                "{\"type\":\"object\",\"properties\":{\"wrongProperty\":{\"type\": null}}}");
+        JsonNode fromJavaType = schemaFactory.createSchema(NullValueOnFieldExample.class);
+        System.out.println(MAPPER.writeValueAsString(fromJavaType));
+        assertEquals(fromResource, fromJavaType);
+    }
+
     public void testMultilineValue() throws Throwable {
         JsonNode fromResource = MAPPER.readTree(
                 "{\"type\":\"object\",\"properties\":{\"wrongProperty\":{\"type\":\"string\",\"k\":{\"VALID\":\"VALID\"}}}}");
         JsonNode fromJavaType = schemaFactory.createSchema(MultilineValueExample.class);
         System.out.println(MAPPER.writeValueAsString(fromJavaType));
+        assertEquals(fromResource, fromJavaType);
+    }
 
+    public void testJsonPrimitives() throws Throwable {
+        JsonNode fromResource = MAPPER.readTree(
+                "{\"b\":true,\"d\":3.141,\"f\":-0.1,\"i\":-1,\"l\":42}");
+        JsonNode fromJavaType = schemaFactory.createSchema(PrimitiveExample.class);
+        System.out.println(MAPPER.writeValueAsString(fromJavaType));
+        assertEquals(fromResource, fromJavaType);
+    }
+
+    public void testArrayInPath() throws Throwable {
+        JsonNode fromResource = MAPPER.readTree("{\"a\":[null,11,22,33]}");
+        JsonNode fromJavaType = schemaFactory.createSchema(ArrayInPathExample.class);
+        System.out.println(MAPPER.writeValueAsString(fromJavaType));
+        assertEquals(fromResource, fromJavaType);
+    }
+
+    public void testArrayInValue() throws Throwable {
+        JsonNode fromResource = MAPPER.readTree("{\"a\":[null,11,22,33]}");
+        JsonNode fromJavaType = schemaFactory.createSchema(ArrayInValueExample.class);
+        System.out.println(MAPPER.writeValueAsString(fromJavaType));
+        assertEquals(fromResource, fromJavaType);
+    }
+
+    public void testObjectInPath() throws Throwable {
+        JsonNode fromResource = MAPPER.readTree("{\"o\":{\"foo\":11,\"bar\":22}}");
+        JsonNode fromJavaType = schemaFactory.createSchema(ObjectInPathExample.class);
+        System.out.println(MAPPER.writeValueAsString(fromJavaType));
+        assertEquals(fromResource, fromJavaType);
+    }
+
+    public void testObjectInValue() throws Throwable {
+        JsonNode fromResource = MAPPER.readTree("{\"o\":{\"foo\":11,\"bar\":22}}");
+        JsonNode fromJavaType = schemaFactory.createSchema(ObjectInValueExample.class);
+        System.out.println(MAPPER.writeValueAsString(fromJavaType));
         assertEquals(fromResource, fromJavaType);
     }
 
     // -----------------------------------------------------------------------
+
+    @XProperties({ "type = null" })
+    public static class NullValueOnClassExample {
+    }
+
+    public static class NullValueOnFieldExample {
+        @XProperties({ "type = null" })
+        private String wrongProperty;
+
+        public String getWrongProperty() {
+            return wrongProperty;
+        }
+
+        public void setWrongProperty(String wrongProperty) {
+            this.wrongProperty = wrongProperty;
+        }
+    }
 
     public static class MultilineValueExample {
         @XProperties({ "k={\\\n\"VALID\":\\\n\"VALID\"\\\n}" })
@@ -41,5 +108,47 @@ public class ValidPropertyTest extends TestCase {
         public void setWrongProperty(String wrongProperty) {
             this.wrongProperty = wrongProperty;
         }
+    }
+
+    @XProperties({
+            "type = null",
+            "b = true",
+            "d = 3.141",
+            "f = -0.1",
+            "i = -1",
+            "l = 42"
+    })
+    public static class PrimitiveExample {
+    }
+
+    @XProperties({
+            "type = null",
+            "a.3 = 33",
+            "a.2 = 22",
+            "a.1 = 11"
+    })
+    public static class ArrayInPathExample {
+    }
+
+    @XProperties({
+            "type = null",
+            "a = [null, 11, 22, 33]"
+    })
+    public static class ArrayInValueExample {
+    }
+
+    @XProperties({
+            "type = null",
+            "o.foo = 11",
+            "o.bar = 22"
+    })
+    public static class ObjectInPathExample {
+    }
+
+    @XProperties({
+            "type = null",
+            "o = { \"foo\": 11, \"bar\": 22 }"
+    })
+    public static class ObjectInValueExample {
     }
 }
